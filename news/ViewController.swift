6502885cbf30fea2b49fb4859bdd03fd053768cd
ran kellner <rank4545@gmail.com>
2022-02-09 20:25:00 +0200

    Initial Commit

---
+++ news/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news
+//
+//  Created by Ran Kellner on 09/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
